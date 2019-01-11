52fbf0819f0e00ec285e0a099e08763a956aeb33
Fukuta Keisuke <fukuta6140@gmail.com>
2019-01-11 12:58:44 +0900

    asdf
    asdf

---
+++ Atcoder/abc098d.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = [int(x) for x in input().split()]
+
+r = 0
+ref = 0
+ans = 0
+for l in range(N):
+    while r < N and ref & A[r] == 0:
+        ref ^= A[r]
+        r += 1
+
+    ans += r - l
+
+    if r == l:
+        r += 1
+    else:
+        ref ^= A[l]
+
+print(ans)
