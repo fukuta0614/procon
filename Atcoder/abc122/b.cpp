e3ace9265e5ba825aa7056495d3fce97fd3edf50
Keisuke Fukuta <fukuta-k@keyence.co.jp>
2019-04-03 14:12:10 +0900

    abc122

---
+++ Atcoder/abc122/b.cpp
@@ -0,0 +1,34 @@
+// abc122_b
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include "../cxx-prettyprint/prettyprint.hpp"
+#endif
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> P;
+
+#define REP(i, n) for (int (i) = 0 ; (i) < (int)(n) ; ++(i))
+#define REPN(i, m, n) for (int (i) = m ; (i) < (int)(n) ; ++(i))
+#define REP_REV(i, n) for (int (i) = (int)(n) - 1 ; (i) >= 0 ; --(i))
+#define INF ((1 << 29)-1)
+#define MOD (1000000007)
+
+#define ALL(x) x.begin(), x.end()
+#define COUT(x) cout << x << endl
+#define COUT_2DARR(h, w, arr) REP(i, h) { REP(j, w) cout << arr[i][j] << " "; cout << endl; }
+
+struct PreMain {PreMain(){std::cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<setprecision(20);}} premain;
+
+int main() {
+#ifdef LOCAL
+    std::ifstream in("../arg.txt");
+    std::cin.rdbuf(in.rdbuf());
+#endif
+
+    int N;
+    cin >> N;
+
+    return 0;
+}
