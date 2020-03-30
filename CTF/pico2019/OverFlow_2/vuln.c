417bf61cb843c5b7ee1fc75fee56cd8ee6a37082
fukuta0614 <fukuta6140@gmail.com>
2020-03-31 07:14:29 +0900

    picopico

--- CTF/pico2019/OverFlow_2/vuln.c
+++ CTF/pico2019/OverFlow_2/vuln.c
@@ -41,3 +41,4 @@ int main(int argc, char **argv){
   return 0;
 }
 
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\xe6\x85\x04\x08
